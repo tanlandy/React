b7cf3cf2a6b2940e53c6225f116e790aaa02db1a
Kunlin <tanlandy6@gmail.com>
2022-03-27 12:17:37 -0700

    keeper part 1

---
+++ Keeper-app/keeper-app-part-1/src/components/Note.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+function Note() {
+  return (
+    <div className="note">
+      <hi1>This is the title.</hi1>
+      <p>This is the paragraph.</p>
+    </div>
+  );
+}
+
+export default Note;
